45d9318cfd8a27e387f60ca119599e7cb0b4eb37
lpstudy <lpstudy@qq.com>
2018-11-27 22:27:32 +0800

    add is_data_loss_with_m_fails

--- mttdlsim_3fuben.cpp
+++ mttdlsim_3fuben.cpp
@@ -6,6 +6,9 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_statistics.h>
+#include <iostream>
+#include <vector>
+using std::vector;
 
 // Total max disk numbers
 #define MAX_DISKS 50000
@@ -23,7 +26,8 @@
 // data coding mode
 #define Twocopy 0
 #define Threecopy 1
-#define RS 2
+#define RShorizontal 2 // 水平放置
+#define RSvertical 3
 
 // event and disk structure
 typedef struct
@@ -82,7 +86,7 @@ extern void heap_delete(int i);
 
 // data loss functions
 extern int Threecopy_failed(int d, int n, int r);
-array_failed func[] = { Threecopy_failed, Threecopy_failed };
+array_failed func[] = { Threecopy_failed, Threecopy_failed, Threecopy_failed,Threecopy_failed,Threecopy_failed };
 
 /*****************************************************
  * Global variables which can be used anywhere
@@ -95,6 +99,142 @@ double Lgama_rest, Lyita_rest, Lbita_rest; // disk repair
 // warn_disks的存在感觉没有价值，并不会用来判断data_loss
 disk_array failed_disks, warn_disks, ld_disks, failed_nodes;
 
+/*****************************************************
+* Data loss functions
+****************************************************/
+template <typename T>
+void combine_inner(T &data, int start, int n, int m, int depth, T temp, vector<T> &result)
+{
+	if (depth == m - 1)
+	{
+		//最内层循环 将temp加入result
+		for (int i = start; i < n - (m - depth - 1); ++i)
+		{
+			temp[depth] = data[i];
+			result.push_back(temp);
+		}
+	}
+	else
+		for (int i = start; i < n - (m - depth - 1); ++i)
+		{
+			temp[depth] = data[i];//每层输出一个元素
+			combine_inner(data, i + 1, n, m, depth + 1, temp, result);
+		}
+}
+
+// 返回从k中选出m个的下标值
+//combine(4, -1); combine(4, 0); combine(4, 1); combine(4, 2); combine(4, 3); combine(4, 4); combine(4, 5);
+vector<vector<int>> combine(int k, int m)
+{
+	if (m <= 0 || m > k)
+		return{};
+	vector<int> data;
+	for (int i = 0; i < k; ++i) data.push_back(i);
+	if (k == m) return{ data };
+	int depth = 0;
+	vector<vector<int>> result;
+	vector<int> temp(m, 0);
+	combine_inner(data, 0, data.size(), m, depth, temp, result);
+	printf("k:%d, m:%d, size:%d\n", k, m, result.size());
+	for (auto it : result) {
+		for (auto i : it) printf("%d ", i);
+		printf("\n");
+	}
+	return result;
+}
+// 从故障数组中选择m个故障
+vector<vector<disk_struct>> combine_darray(disk_array& darray, int m) {
+	vector<vector<disk_struct>> results;
+	int k = (darray.end + MAX_DISKS - darray.front) % MAX_DISKS;
+	vector<vector<int>> coms = combine(k, m);
+	for (vector<int> vi : coms) {
+		vector<disk_struct> res;
+		// 例如index为(0,1,3)，那么将drray中第0,1,3个元素拿出来放入res中
+		for (int index : vi) {
+			res.push_back(darray.disks[(index + darray.front) % MAX_DISKS]);
+		}
+		results.push_back(res);
+	}
+	return results;
+}
+
+//根据传入的编码方案，以及故障的磁盘数目failed_d_num和节点数据failed_n_num，计算数据丢失概率
+double func_probability(int coding, const vector<disk_struct>& failed_d_num, 
+	const vector<disk_struct>& failed_n_num, disk_struct& disk, bool is_all_nodes_failre) {
+	// 跳过不包含当前disk的磁盘
+	bool is_find = false;
+	for (auto it : failed_d_num) {
+		if (it == disk) {
+			is_find = true;
+			break;
+		}
+	}
+	for (auto it : failed_n_num) {
+		if (it == disk) {
+			is_find = true;
+			break;
+		}
+	}
+	// 我们需要跳过不包含当前故障的disk的磁盘
+	if (!is_find) {
+		return 0.0; 
+	}
+
+
+	if (coding == RShorizontal) {
+
+	}
+	else if (coding == RSvertical) {
+
+	}
+	else {
+		printf("ERROR coding!");
+		exit(-1);
+	}
+	return 0.0;
+}
+bool data_loss_with_prob(double prob) {
+	if ((rand() % 10000) < prob * 100) {
+		return true;
+	}
+	return false;
+}
+
+// 从node故障和disk故障数组中，每次选择m个故障，然后判定故障情形，并计算这种故障导致的概率
+disk_struct failed[MAX_DISKS];
+bool is_data_loss_with_m_fails(disk_struct disk, int event_type) {
+	// 其实有一种不需要使用跳过disk的方法，也就是说，在求combine的时候修改diskarray的end位置，进行--操作
+	// 然后再把当前的disk加入进去。由于时间关系，先简单的寻找m+1个failed，然后跳过不包含当前故障的disk。
+
+	int m = rs_m + 1;
+	// 1. x个磁盘故障，x > 0, m-x个节点故障
+	for (int x = 1; x <= m; ++x) {
+		int n_nums = m - x;
+		vector<vector<disk_struct>> disks_vec = combine_darray(failed_disks, x);
+		vector<vector<disk_struct>> nodes_vec = combine_darray(failed_nodes, n_nums);
+
+		// 选择x个磁盘和m-x个节点故障，并判断是否发生数据丢失
+		for (auto i : disks_vec) {
+			for (auto j : nodes_vec) {
+				double prob = func_probability(coding, i, j, disk, false);
+				if (data_loss_with_prob(prob)) return true;
+			}
+		}
+	}
+	// 2. m个节点故障
+	vector<vector<disk_struct>> nodes_vec = combine_darray(failed_nodes, m);
+	vector<disk_struct> disks_vec;
+	for (auto j : nodes_vec) {
+		double prob = func_probability(coding, disks_vec, j, disk, false);
+		if (data_loss_with_prob(prob)) return true;
+	}
+	return false;
+}
+
+
+/*****************************************************
+* Basic event functions for disk and node
+****************************************************/
 inline void events_for_new_disk(disk_struct d)
 {
     int j = d.disk_no, k = d.node_no, i = d.rack_no;
@@ -343,7 +483,9 @@ double sim_num_loss(double t)
 
 int main(int argc, char *argv[])
 {
-    double result[10000];
+	
+    
+	double result[10000];
     int i, reps1, num_dloss, reps2, add_reps1 = 0, add_loss = 0;
     double pro_loss, t;
 
@@ -358,7 +500,7 @@ int main(int argc, char *argv[])
 	else if (!strcmp(argv[1], "Threecopy"))
 		coding = Threecopy;
 	else if (!strcmp(argv[1], "RS"))
-		coding = RS;
+		coding = RShorizontal;
 	else
     {
         printf("coding BUG\n");
@@ -394,10 +536,8 @@ int main(int argc, char *argv[])
     fdr = atof(argv[22]); // 80
     t = atof(argv[23]); // 43800 5年
     reps2 = atof(argv[24]); // 10
-	if (coding == RS) {
-		rs_k = atoi(argv[25]); // rs_k
-		rs_m = atoi(argv[26]);
-	}
+	rs_k = atoi(argv[25]); // rs_k
+	rs_m = atoi(argv[26]);
 	fprintf(stderr, "disk:%d node:%d rack:%d fdr:%.0f coding:%s(%d,%d) t:%.1fyear reps:%.0f\n", 
 		disk, node, rack, fdr, argv[1], rs_k, rs_m, t/24/365, reps2);
     srand((unsigned)time(NULL));
